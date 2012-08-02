d68cbfcaf0724969514494aaa514487f311ac2c8
tzhang <skineffect@163.com>
2012-08-02 05:53:51 -0400

    single check radio and made event creating initialy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,21 @@
 class EventsController < ApplicationController
+	def new
+		@event=Event.new
+		@title="New Event"
+	end
+	
 	def index
 		@events = Event.all
 		@title = "Events"
-  end
-
+  	end
+  	
+  	def create
+  		@event = Event.new(params[:event])
+  		if @event.save
+  		    # flash message here
+  		else
+  		   @title = "New Event"
+  		   render new
+  		end
+  	end
 end
